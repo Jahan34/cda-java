5be211c301129903f492f88ffca3d5a532ea9930
Jah.serra <Jah.serra@s124-14.pedago.mermoz.edu>
2019-11-04 17:51:21 +0100

    add tp3

--- src/tp3/Tp3Exo1.java
+++ src/tp3/Tp3Exo1.java
@@ -1,5 +1,11 @@
 package tp3;
-
+import java.util.Scanner;
 public class Tp3Exo1 {
-
+Scanner booleen = new Scanner (System.in);
+boolean valeur;
+system.out.print("saisir un booleen");
+valeur = boolean.nextBoolean(false);
+system.out.print("booleen saisi = "+valeur);
+booleen.close();
+	}
 }
