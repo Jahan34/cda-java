a1f945c73bdab6e2b6fb2895066489b793aa4129
Jah.serra <Jah.serra@s124-14.pedago.mermoz.edu>
2019-11-06 12:51:15 +0100

    add Tp3Exo1

--- src/tp3/Tp3Exo1.java
+++ src/tp3/Tp3Exo1.java
@@ -1,11 +1,49 @@
 package tp3;
 import java.util.Scanner;
 public class Tp3Exo1 {
-Scanner booleen = new Scanner (System.in);
-boolean valeur;
-system.out.print("saisir un booleen");
-valeur = boolean.nextBoolean(false);
-system.out.print("booleen saisi = "+valeur);
-booleen.close();
+	public static <prooch> void main(String[]args){
+	Scanner verif = new Scanner (System.in);
+	boolean valeur;
+	System.out.println("saisir un booleen: ");
+	valeur = verif.nextBoolean();
+	System.out.println("booleen saisi = "+valeur);
+	
+	byte byby;
+	System.out.println("saisir un byte: ");
+	byby = verif.nextByte();
+	System.out.println("byte saisi = "+byby);
+	
+	short tourist;
+	System.out.println("saisir un short: ");
+	tourist = verif.nextShort();
+	System.out.println("short saisi = "+tourist);
+	
+	int inte;
+	System.out.println("saisir un int: ");
+	inte = verif.nextInt();
+	System.out.println("int saisi = "+inte);
+	
+	long lo;
+	System.out.println("saisir un long: ");
+	lo = verif.nextLong();
+	System.out.println("long saisi = "+lo);
+
+	float reel;
+	System.out.println("saisir un reel: ");
+	reel = verif.nextFloat();
+	System.out.println("reel saisi = "+reel);
+	
+	double db;
+	System.out.println("saisir un double: ");
+	db = verif.nextDouble();
+	System.out.println("double saisi= "+db);
+	
+	char cha;
+	System.out.println("saisir un char: ");
+	cha = verif.next().charAt(0);
+	System.out.println("char saisi = "+cha);
+	
+	verif.close();
 	}
 }
+
