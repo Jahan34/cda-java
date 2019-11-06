2d96839ef4df963e05fda26e81c861b1056d9e91
Jah.serra <Jah.serra@s124-14.pedago.mermoz.edu>
2019-11-06 16:34:31 +0100

    add tp3 exo4

--- src/tp3/Tp3Exo4.java
+++ src/tp3/Tp3Exo4.java
@@ -1,10 +1,26 @@
 package tp3;
-
+import java.util.Scanner;
 public class Tp3Exo4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Scanner algo=new
+	Scanner(System.in);
+		
+	int seconde=1;//System.out.println("Saisir des secondes : ");
+	//seconde =algo.nextInt();
+	//System.out.println("Entier saisi = "+seconde);
 
+	int minute= seconde*60;System.out.println("Saisir des minutes : ");
+	minute =algo.nextInt();
+	System.out.println("resultat = "+minute*60+" secondes");
+	
+	int heure= seconde*60*60;System.out.println("Saisir des heures : ");
+	heure =algo.nextInt();
+	System.out.println("resultat = "+heure*60*60+" secondes");
+	
+	
+	algo.close();
 	}
 
 }
