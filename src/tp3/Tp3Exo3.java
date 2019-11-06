b9b38ebae26a075c0bbdb848f425a1d6184411eb
Jah.serra <Jah.serra@s124-14.pedago.mermoz.edu>
2019-11-06 15:32:59 +0100

    add exo3tp3

--- src/tp3/Tp3Exo3.java
+++ src/tp3/Tp3Exo3.java
@@ -4,7 +4,11 @@ public class Tp3Exo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	int n=7,p=4;int negatif = -32000;System.out.println("n&p,n|p,n^p = " + (n&p) +" "+ (n|p) + " " + (n^p));
+	System.out.println("n>>2,n>>4,n<<2 = " + (n>>2) +" "+ (n>>4) + " " + (n<<2));
+	System.out.println("negatif>>2, negatif>>4 = " + (negatif>>2) +" "+(negatif>>4));
+	System.out.println("negatif>>>2, negatif>>>4 = " + (negatif>>>2) +" "+ (negatif>>>4));
+	// utilisation de bit decallage gauche ou droite pour remplacer les 0
 	}
 
 }
